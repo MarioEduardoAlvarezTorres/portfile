19e8c1fa4b053c5c6af7240dfda2aa2df9044b66
Mario Eduardo <eduardoat44@gmail.com>
2023-06-02 09:47:03 -0600

    Configuracion de DTO

--- src/projects/dto/create-project.dto.ts
+++ src/projects/dto/create-project.dto.ts
@@ -1,1 +1,29 @@
-export class CreateProjectDto {}
+import { IsArray, IsIn, IsString, MinLength } from 'class-validator';
+
+export class CreateProjectDto {
+  @IsString()
+  @MinLength(5)
+  name: string;
+
+  @IsString()
+  @MinLength(5)
+  description: string;
+
+  @IsString({ each: true })
+  @IsIn(['express', 'nestjs'], { each: true })
+  @IsArray()
+  developerToolsBackend: string[];
+
+  @IsString({ each: true })
+  @IsIn(['css', 'sass'], { each: true })
+  @IsArray()
+  developerToolsFrontend: string[];
+
+  @IsString()
+  @MinLength(5)
+  link: string;
+
+  @IsString({ each: true })
+  @IsArray()
+  images: string[];
+}
