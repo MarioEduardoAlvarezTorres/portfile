68b99ea7c1aa1a00543bee3dfa298544cb587521
Mario Eduardo <eduardoat44@gmail.com>
2023-06-02 09:25:04 -0600

    Creacion de entidades Project y ProjectImage

--- src/projects/entities/project.entity.ts
+++ src/projects/entities/project.entity.ts
@@ -1,1 +1,29 @@
-export class Project {}
+import { Column, Entity, OneToMany, PrimaryGeneratedColumn } from 'typeorm';
+import { ProjectImage } from './project-image.entity';
+
+@Entity()
+export class Project {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column()
+  name: string;
+
+  @Column()
+  description: string;
+
+  @Column('simple-array')
+  developerToolsBackend: string[];
+
+  @Column('simple-array')
+  developerToolsFrontend: string[];
+
+  @Column()
+  link: string;
+
+  @OneToMany(() => ProjectImage, (projectImage) => projectImage.project, {
+    cascade: true,
+    eager: true,
+  })
+  images: ProjectImage[];
+}
